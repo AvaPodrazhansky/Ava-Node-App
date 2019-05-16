ab580d359b0a675b6dd0a9524c287b2460e3403d
AvaPodrazhansky <avapod@gmail.com>
2019-05-16 17:32:23 -0400

    Minor formatting change

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 5000
 
 app.use(express.static('public'))
 
-app.listen(process.env.PORT || 8080, () => console.log('We Gucci'))
+app.listen(PORT, () => console.log('We Gucci'))
