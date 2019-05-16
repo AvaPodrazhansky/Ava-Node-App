040b9700e8fba2c792a7004240ce9df8103f00fb
AvaPodrazhansky <avapod@gmail.com>
2019-05-16 18:03:39 -0400

    Added changes to the server

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
+// app.set('view engine', 'html')
+// app.get('/', (req, res) => res.render('index'))
+// app.get('/page2', (req, res) => res.render('page2'))
 
 app.listen(PORT, () => console.log('We Gucci'))
