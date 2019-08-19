f6b968d7b50193c0d906fd8771e2434d1ee53cec
AvaPodrazhansky <avapod@gmail.com>
2019-08-19 13:33:20 -0400

    Added counter

--- public/index-functions.js
+++ public/index-functions.js
@@ -1,3 +1,19 @@
 function clickedTheButton() {
     document.getElementById("myButton").innerHTML = "You clicked me";
 }
+
+let count = 0;
+
+function getCount(){
+    document.getElementById("count").innerHTML = count;
+}
+
+function increment(){
+    count = count + 1;
+    getCount();
+}
+
+function decrement(){
+    count = count -1;
+    getCount();
+}
